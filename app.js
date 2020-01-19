034b5ad4644e88f44503a70f11dc0d85e3a913ae
Ambeshgaurav <38497230+Ambeshgaurav@users.noreply.github.com>
2020-01-19 13:59:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,3 @@
-// const express=require("express");
-// const app=express();
-// const routes=require('./router/routes')
-// app.use(express.json());
-
-// app.use('/customer',routes);
-// app.listen(3099);
-// console.log("server running at the port number 3099");
-
-
-
-
-
-
 const express=require("express");
 const app=express();
 const routes=require('./routes/clientRoutes')
